bef503a37670d678ef63053e692b08d5939c12cd
CSU <CSU@CSU-TOSH>
2018-11-05 11:33:00 +0100

    Formatiranje stringa

--- src/stringovi/Demo.java
+++ src/stringovi/Demo.java
@@ -1,24 +1,31 @@
 package stringovi;
 
+import java.lang.annotation.Documented;
+import java.text.NumberFormat;
+
 public class Demo {
 
 	public static void main(String[] args) {
-		String s1  = "danas";
-		String s4  = "Danas";
-		String s2 = " je lep dan";
-		String s3 = s1 +s2;
-		System.out.println(s3);
-
-//		System.out.println(s3.substring(0,5));
-		System.out.println("Da li je jednako " 
-					+ s1.equalsIgnoreCase(s4));
+		String s1 = "Danas je lep dan";
+		String s2 = "Danas je suncan dan";
+		boolean b = s1.regionMatches(0, s2, 0, 9);
+		System.out.println(b);
 		
-		System.out.println(s3.indexOf("an"));
-		System.out.println(s3.lastIndexOf("an"));
+		System.out.println(s1.startsWith(" je", 5));
+		System.out.println(s1.endsWith("suncan dan"));
 		
-		char[] niz = s3.toCharArray();
-		System.out.println(niz.length);
-
+		System.out.println(s2.compareTo(s1));
+//		
+//		Integer i = Integer.valueOf("58D");
+//		String s6 = String.valueOf(i);
+		
+		double d = 100.0/3;
+		System.out.println(d);
+		
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(5);
+		String s = nf.format(d);
+		System.out.println(s);
 	}
 
 }
