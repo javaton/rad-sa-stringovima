e96d6b7ab1c77bbb7553f80401aaa2c7b6c3201b
CSU <CSU@CSU-TOSH>
2018-11-05 11:02:12 +0100

    equalsIgnoreCase

--- src/stringovi/Demo.java
+++ src/stringovi/Demo.java
@@ -3,13 +3,15 @@ package stringovi;
 public class Demo {
 
 	public static void main(String[] args) {
-		String s1  = "Danas";
+		String s1  = "danas";
+		String s4  = "Danas";
 		String s2 = " je lep dan";
 		String s3 = s1 +s2;
 		System.out.println(s3);
 
-		System.out.println(s3.substring(0,5));
-		System.out.println("Da li je jednako " + s1.equals(s3.substring(0,5)));
+//		System.out.println(s3.substring(0,5));
+		System.out.println("Da li je jednako " 
+					+ s1.equalsIgnoreCase(s4));
 		
 		System.out.println(s3.indexOf("an"));
 		System.out.println(s3.lastIndexOf("an"));
