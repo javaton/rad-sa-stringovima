46fda4ddfb72121b727616e77a600c0e8ca3ed86
CSU <CSU@CSU-TOSH>
2018-11-05 11:52:52 +0100

    StringBuffer

--- src/stringovi/Demo.java
+++ src/stringovi/Demo.java
@@ -8,24 +8,20 @@ public class Demo {
 	public static void main(String[] args) {
 		String s1 = "Danas je lep dan";
 		String s2 = "Danas je suncan dan";
-		boolean b = s1.regionMatches(0, s2, 0, 9);
-		System.out.println(b);
 		
-		System.out.println(s1.startsWith(" je", 5));
-		System.out.println(s1.endsWith("suncan dan"));
+		StringBuffer bf = new StringBuffer();
+		bf.append(s1);
+		bf.append(". ");
+		bf.append(s2);
 		
-		System.out.println(s2.compareTo(s1));
-//		
-//		Integer i = Integer.valueOf("58D");
-//		String s6 = String.valueOf(i);
+		System.out.println(bf);
 		
-		double d = 100.0/3;
-		System.out.println(d);
+		bf.delete(5, 8);
+		bf.insert(5, " nije");
 		
-		NumberFormat nf = NumberFormat.getInstance();
-		nf.setMaximumFractionDigits(5);
-		String s = nf.format(d);
-		System.out.println(s);
+	
+		System.out.println(bf);
+		System.out.println(bf.reverse());
 	}
 
 }
